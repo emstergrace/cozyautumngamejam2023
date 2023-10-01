39195d71caef9884a1d2da96ca14c40370c50dda
OnlyFeatures <soren.j.ludwig@gmail.com>
2023-09-30 17:55:18 -0700

    Clean up controls

--- My project/Assets/Scripts/Gameplay/PlayerController.cs
+++ My project/Assets/Scripts/Gameplay/PlayerController.cs
@@ -6,11 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     private FrameInput Input;
     private float MovementSpeed = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
