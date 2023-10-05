143ee0ad2f241bbb1479651d3c6dae0203cf3c2a
sgrabar <grabar.svetlana@gmail.com>
2023-10-05 16:13:18 -0700

    more menus

--- My project/Assets/Scripts/UI/Main Menu.cs
+++ My project/Assets/Scripts/UI/Main Menu.cs
@@ -19,6 +19,12 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene("Scenes/UI/Credits");
     }
 
+    public void OnMainMenuButton()
+    {
+        Debug.Log("I'm here");
+        SceneManager.LoadScene("Scenes/UI/Main Menu");
+    }
+
     public void OnQuitButton()
     {
         Application.Quit();
